f00c8134c3d1334f1dc692036c7d97820472a15f
Safi Raza <safiraza004@gmail.com>
2024-08-11 13:59:55 +0530

    Time: 0 ms (100%), Space: 40 MB (75.5%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public int fib(int n) {
+        int[] dp  = new int[n+1];
+        Arrays.fill(dp,-1);
+        int ans = fibhelper(n,dp);
+        return ans;
+    }
+    public int fibhelper(int n, int[] dp){
+        if(n==0||n==1){return n;}
+        if(dp[n]!=-1){return dp[n];}
+
+        dp[n] = fibhelper(n-1,dp)+fibhelper(n-2,dp);
+        return fibhelper(n-1,dp)+fibhelper(n-2,dp);
+    }
+}
