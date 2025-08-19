113d145368e27728976716516f4be69c87864a7f
Mohd Safi Raza <safiraza004@gmail.com>
2025-08-19 09:47:44 +0530

    Time: 2 ms (92.57%), Space: 47.7 MB (6.42%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,32 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // int[] res = new int[2];
-        // int i=0;
-        // int j=nums.length-1;
-        // while(i<j){
-        //     int mid = (i+j)/2;
-        //     if(nums[i]+nums[mid]>target){
-        //         j=mid-1;
-        //     }else if(nums[i]+nums[mid]<target){
-        //         i=mid+1;
-        //     }else{
-        //         return new int[] {i+1,mid+1};
-        //     }
-        // }
-        // return new int[] {};
-
-        //in this questions we can use binary search but i am using two pointers
-
         int i = 0;
         int j = nums.length-1;
         while(i<j){
-            int curr = nums[i]+nums[j];
-            if(curr==target){
-                return new int[] {i+1,j+1};
-            }else if (curr>target){
+            if(nums[i]+nums[j]>target){
                 j--;
-            }else{
+            }else if(nums[i]+nums[j]<target){
                 i++;
+            }else{
+                return new int[] {i+1,j+1};
             }
         }
         return new int[] {};
