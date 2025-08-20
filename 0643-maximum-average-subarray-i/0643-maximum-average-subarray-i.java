f506d7d82dbb0585c229d1f6800180c4e6574a41
Mohd Safi Raza <safiraza004@gmail.com>
2025-08-20 10:07:30 +0530

    Time: 5 ms (47.22%), Space: 55.5 MB (96.33%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -10,15 +10,15 @@ class Solution {
         //     ans  = Math.max(ans,sum/k);
         // }
         // return ans;
-        double sum  = 0;
+        double sum = 0;
         for(int i=0;i<k;i++){
-            sum += nums[i];
+            sum+=nums[i];
         }
-        double maxSum = sum;
-        for(int i =k;i<nums.length;i++){
+        double max = sum;
+        for(int i=k;i<nums.length;i++){
             sum += nums[i]-nums[i-k];
-            maxSum = Math.max(maxSum,sum);
+            max = Math.max(max,sum);
         }
-        return maxSum / k;
+        return max / k;
     }
 }
