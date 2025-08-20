fa9d16ad346c73fcff7b11f7d7eee9dd6f49bb6f
Mohd Safi Raza <safiraza004@gmail.com>
2025-08-20 10:26:52 +0530

    Time: 6 ms (73.16%), Space: 44.8 MB (57.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,39 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        /* //using hashmap
-        int count =0;
-        HashMap<Character,Integer> hm = new HashMap<>();
-        int i=0;
-        int j=0;
-        while(j<s.length()){
-            hm.put(s.charAt(j),hm.getOrDefault(s.charAt(j),0)+1);
-            if(hm.size() == j-i+1){
-                count = Math.max(count,j-i+1);
-                j++;
-            }else if(hm.size() < j-i+1){
-                while(hm.size() < j-i+1){
-                    hm.put(s.charAt(i),hm.get(s.charAt(i))-1);
-                    if(hm.get(s.charAt(i))==0) hm.remove(s.charAt(i));
-                    i++;
-                }
-                j++;
-            }
+       int left = 0; 
+       int right = 0;
+       int ans= 0;
+       Set<Character> set = new HashSet<>();
+       while(right<s.length()){
+        char c = s.charAt(right);
+        if(!set.contains(c)){
+            set.add(c);
+            ans = Math.max(ans,right-left+1);
+            right++;
+        }else{
+            set.remove(s.charAt(left));
+            left++;
         }
-        
-        return count;*/  //using hashset
-        int ans=0;
-        Set<Character> hs = new HashSet<>();
-        int i=0; int j=0;
-        while( j<s.length() ){
-            if(!hs.contains(s.charAt(j))){
-                hs.add(s.charAt(j));
-                j++;
-                ans = Math.max(ans,hs.size());
-            }else{
-                hs.remove(s.charAt(i));
-                i++;
-            }
-        }
-        return ans;
+       }
+       return ans;
     }
 }
