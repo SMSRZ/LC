f1a6a8c792ecb90facac10a03ecd9735d1cbba9c
Mohd Safi Raza <safiraza004@gmail.com>
2025-11-15 22:08:08 +0530

    Time: 0 ms (100%), Space: 47.9 MB (15.7%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,28 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[]{-1,-1};
         int low=0;
-        int high=nums.length-1;
+        int[] arr = {-1,-1};
+        int high = nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid = (low+high)/2;
             if(nums[mid]<=target){
                 low=mid+1;
-                
             }else{
                 high=mid-1;
             }
-            if(nums[mid]==target){
-                ans[1]=mid;
-            }
+            if(nums[mid]==target) arr[1]=mid;
         }
         low=0;
-        high=nums.length-1;
+        high = nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid = (low+high)/2;
             if(nums[mid]>=target){
                 high=mid-1;
-                
             }else{
                 low=mid+1;
             }
-            if(nums[mid]==target){
-                ans[0]=mid;
-            }
+            if(nums[mid]==target) arr[0]=mid;
         }
-        return ans;
+        return arr;   
     }
 }
-        /* iterative approach
-        int[] ans = new int[]{-1,-1};
-        
-        for(int sp=0; sp<nums.length; sp++){
-            if(nums[sp]==target){
-                ans[0]=sp;
-                break;
-            }
-        }
-        for(int ep=nums.length-1; ep>=0; ep--){
-            if(nums[ep]==target){
-                ans[1]=ep;
-                break;
-            }
-        }
-        return ans;
-        //i have currently not learned binary search but i have solved it in o(n) time above i will revisit it later
-    }
-}*/
