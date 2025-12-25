3ddd012e1000974542435af7e7d7b40295e27d8e
Mohd Safi Raza <safiraza004@gmail.com>
2025-12-25 11:04:10 +0530

    Time: 12 ms (86.53%), Space: 47.5 MB (20.08%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,18 +1,45 @@
 class Solution {
     public String frequencySort(String s) {
+        //solution1 hashmap + priority queue
+        // HashMap<Character,Integer> hm = new HashMap<>();
+        // for(char c : s.toCharArray()){
+        //     hm.put(c,hm.getOrDefault(c,0)+1);
+        // }
+        // PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> hm.get(b)-hm.get(a));
+        // pq.addAll(hm.keySet());
+
+        // StringBuilder sb = new StringBuilder();
+        // while(!pq.isEmpty()){
+        //     char c = pq.poll();
+        //     int count = hm.get(c);
+        //     while(count-- > 0){
+        //         sb.append(c);
+        //     }
+        // }
+        // return sb.toString();
+
+        //solution 2 bucket sort
+
         HashMap<Character,Integer> hm = new HashMap<>();
         for(char c : s.toCharArray()){
             hm.put(c,hm.getOrDefault(c,0)+1);
         }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> hm.get(b)-hm.get(a));
-        pq.addAll(hm.keySet());
-
+        List<Character>[] buckets = new List[s.length()+1];
+        for(char c : hm.keySet()){
+            int f = hm.get(c);
+            if(buckets[f]==null){
+                buckets[f]=new ArrayList<>();
+            }
+            buckets[f].add(c);
+        }        
         StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            char c = pq.poll();
-            int count = hm.get(c);
-            while(count-- > 0){
-                sb.append(c);
+        for(int i = buckets.length-1;i>=0;i--){
+            if(buckets[i]!=null){
+                for (char c : buckets[i]){
+                    for(int j=0;j<i;j++){
+                        sb.append(c);
+                    }
+                }
             }
         }
         return sb.toString();
