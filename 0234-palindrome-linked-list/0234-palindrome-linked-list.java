71e04435b185905730e895c8f2815a93098a45b3
Mohd Safi Raza <safiraza004@gmail.com>
2025-12-27 11:59:20 +0530

    Time: 4 ms (81.67%), Space: 94.5 MB (45.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,17 +10,54 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack();
-        ListNode temp = head;
-        while(temp != null){
-            st.push(temp.val);
+        //this is the naive sol and this uses a stack to keep the tracks off all elements in a reverse order 
+        // Stack<Integer> st = new Stack();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head ;
+        // while(!st.isEmpty() && temp.next!=null){
+        //     if(st.pop()!=temp.val) return false;
+        //     temp = temp.next;
+        // }
+        // return true;
+        //the correct approach would be Find the middle of linked list , the reverse the second half of the list and then compare 
+
+        
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode secondHalf = reverseList(slow.next);
+
+        // Step 3: Compare
+        ListNode firstHalf = head;
+        ListNode temp = secondHalf;
+        while (temp != null) {
+            if (firstHalf.val != temp.val) return false;
+            firstHalf = firstHalf.next;
             temp = temp.next;
         }
-        temp = head ;
-        while(!st.isEmpty() && temp.next!=null){
-            if(st.pop()!=temp.val) return false;
-            temp = temp.next;
-        }
+
         return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
